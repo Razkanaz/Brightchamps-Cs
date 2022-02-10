d6296125067214014f1a60ff7bfc8808ea2a6fa8
Razkanaz <74030846+Razkanaz@users.noreply.github.com>
2022-02-10 17:02:36 +0700

    Add files via upload

---
+++ Project #3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class program {
+    public static void Main(string[] args) {
+        while (true)
+        {   
+            Console.Write("choose a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the nearest prime number is " + npn(n));
+        }
+    }
+
+    public static int npn(int n) {
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0) {n++; i = 2;}
+        }
+        return n;
+    }
+}
